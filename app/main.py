3a984cc92d4711a89f4ca919394e04310c5fe5f8
mmdaya <132420131+mmdaya@users.noreply.github.com>
2023-09-12 08:57:51 +0530

    sdsad

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def print_ip():
         hostname = socket.gethostname()
         get_ip = socket.gethostbyname(hostname)
         return get_ip
-
+'''
 @app.route('/name')
 def myname():
         return "Dayashankar M M"
@@ -16,6 +16,6 @@ def myname():
 @app.route('/health')
 def health():
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-
+'''
 if __name__ == '_main_':
         app.run(host="0.0.0.0", port="8080")
