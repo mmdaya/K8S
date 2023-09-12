3cea47d1949e54c230646d68ea50823b6e72862c
mmdaya <132420131+mmdaya@users.noreply.github.com>
2023-09-12 08:53:37 +0530

    sdsad

--- app/main.py
+++ app/main.py
@@ -1,21 +1,18 @@
-from flask import Flask
-import socket, json
+from flask import Flask 
+import socket, json 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-        hostname = socket.gethostname()
-        get_ip = socket.gethostbyname(hostname)
-        return get_ip
-
-@app.route('/name')
-def myname():
-        return "Dayashankar M M"
-
+def print_ip():
+    hostname = socket.gethostname()
+    get_ip = socket.gethostbyname(hostname)
+    return get_ip
+  
 @app.route('/health')
-def health():
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+def health_check():
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-if __name__ == '_main_':
-        app.run(host="0.0.0.0", port="8090")
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port="80900")
